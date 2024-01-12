855d699cf04fb65871269a575487368fd0266847
Joseph Juri <joseph.juri@student.tugraz.at>
2024-01-12 20:41:09 +0100

    Started Implementing User creation for Admin

--- src/app/components/admin-page/admin-page.component.ts
+++ src/app/components/admin-page/admin-page.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
   styleUrls: ['./admin-page.component.scss']
 })
-export class AdminPageComponent {
+export class AdminPageComponent implements OnInit {
 
+  username = '';
+  password = '';
+  repeatPassword = '';
+
+  constructor(private router: Router, private snackBar: MatSnackBar){}
+
+  ngOnInit(): void {
+  
+  }
+
+  onAddUserClick() {
+    if(this.password !== this.repeatPassword || this.password.length < 1){
+      this.snackBar.open("Check your passwords","OK");
+      setTimeout( () => { this.snackBar.dismiss() }, 2000 );
+      return
+    }
+
+    /** Add User Logic */
+
+    this.username = '';
+    this.password = '';
+    this.repeatPassword = '';
+    
+  }
+
+  openSnackBar() {
+    
+  }
 }
